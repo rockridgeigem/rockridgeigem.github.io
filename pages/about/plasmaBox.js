13dfa22e624ac5fb42ce6196fa5b80bb9e70d5ee
Rock Ridge iGEM <rockridgeigemvirginia@gmail.com>
2016-05-08 19:41:26 -0400

    Compartmentalizing and added new interface elements for about team

---
+++ pages/about/plasmaBox.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    var localName;
+    var teamImage = {
+        "vignesh": "../../images/team/plas-pics/vignesh_valaboju.png",
+        "shrunal": "../../images/team/plas-pics/shrunal_pothagoni.png",
+        "uday": "../../images/team/plas-pics/uday_sidhu.png",
+        "saavage": "../../images/team/igem_Saavage2.png",
+        "varun": "../../images/team/plas-pics/varun_kulkarni.png"
+    };
+    
+    var team
+    
+    $("#faces-bank img").click(function(){
+        localName = $(this).data('name');
+        if($("#plas-text-container").is(":visible")){
+            $("#plas-text-container").hide();
+        }
+        $("#delta_image").attr("src", teamImage[localName]);
+    });
+    
+    $("#pRoj_text").click(function(){
+        $("#plasmid-text").html("Hi people (click to hide)");
+        $("#plas-text-container").show();
+    });
+    $("#proj").click(function(){
+        $("#plasmid-text").html("Wuzzup fam (click to hide)");
+        $("#plas-text-container").show();
+    });
+    
+    $("#plasmid-text").click(function(){
+        if($("#plas-text-container").is(":visible")){
+            $("#plas-text-container").hide();
+        }
+    });
+});
+
