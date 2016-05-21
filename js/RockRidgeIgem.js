71be671ccf2fdf2a5ccd888e593875bc3dd9a583
Rock Ridge iGEM <rockridgeigemvirginia@gmail.com>
2016-05-20 22:52:58 -0400

    shrinking navbar

---
+++ js/RockRidgeIgem.js
@@ -0,0 +1,13 @@
+
+        
+        $(window).scroll(function(){
+            var m = $(document).scrollTop();
+            if(m > $(".nav-image-wrap").height()){
+                $(".nav-image-wrap").addClass("minify");
+                $(".nav-link").removeClass("nav-link_enlarge");
+            }
+            else{
+                $(".nav-image-wrap").removeClass("minify");
+                $(".nav-link").addClass("nav-link_enlarge");
+            }
+        });
