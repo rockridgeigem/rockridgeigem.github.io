60f2b9b5f55140831191a7a31e5a4f0dc2d5c692
Rock Ridge iGEM <vskred126@gmail.com>
2016-06-28 11:28:24 -0400

    some stuff

---
+++ js/custom/scroll-index.js
@@ -0,0 +1,10 @@
+$(window).scroll(function(){
+    var chg = $(window).scrollTop() % Math.PI;
+    chg /= 2;
+    $("#gear-1").css({
+        transform: 'rotate(' + chg + 'rad)'
+    });
+    $("#gear-2").css({
+        transform: 'rotate(' + (-chg) + 'rad)'
+    });
+});
