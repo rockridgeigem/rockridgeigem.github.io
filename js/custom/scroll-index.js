70194bea2b909d23c92c8f6fce6fb9eb0677b9c6
Rock Ridge iGEM <vskred126@gmail.com>
2016-07-13 11:46:14 -0400

    the geas dont work

--- js/custom/scroll-index.js
+++ js/custom/scroll-index.js
@@ -1,10 +1,13 @@
 $(window).scroll(function(){
     var chg = $(window).scrollTop() % Math.PI;
-    chg /= 2;
+    chg /= 2    ;
     $("#gear-1").css({
         transform: 'rotate(' + chg + 'rad)'
     });
     $("#gear-2").css({
         transform: 'rotate(' + (-chg) + 'rad)'
+    });
+    $("#plas").css({
+        transform: 'rotate(' + chg + 'rad)'
     });
 });
