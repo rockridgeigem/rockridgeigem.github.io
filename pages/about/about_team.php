fb156091447ce4c6e6c6deccfc03eaa214d3fa19
Varun Kulkarni <vskred126@gmail.com>
2016-08-03 14:13:26 -0400

    php?

---
+++ pages/about/about_team.php
@@ -0,0 +1,86 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name = "viewport" content = "width = device-width, initial-scale = 1">
+    <link rel = "icon" type="image/png" href = "../../images/universal/favicon.png" />
+    <title>About the Team</title>
+    
+    <script type = "text/javascript" src = "../../js/jquery/jquery-2.2.3.min.js"></script>
+    <script type = "text/javascript" async src = "../../js/custom/plasma.js"></script>
+    <script type = "text/javascript" async src = "../../js/bootstrap/bootstrap.min.js"></script>
+    
+    <link rel="stylesheet" type="text/css" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../css/styles/stylesheets/screen.css">
+    
+    <link rel = "stylesheet" type="text/css" media="screen and (max-device-width: 800px)" href="../../css/mobile.css" />
+    
+</head>
+
+<body class = "background">
+     <!--<nav class = "navbar navbar-default navbar-static-top">
+       <div class="container-fluid">
+           <div class = "navbar-header">
+               <button type = "button" class = "navbar-toggle" data-toggle = "collapse" data-target = "#nav-button" >
+                   <span class = "sr-only">Toggle Nav</span>
+                   <span class="icon-bar"></span>
+                   <span class="icon-bar"></span>
+                   <span class="icon-bar"></span>
+               </button>
+                <a href = "../../index.html">
+                    <img class = "nav-image-wrap" src = "../../images/universal/igem-nav.png" />
+               </a>
+           </div>
+
+           <div class = "collapse navbar-collapse" id = "nav-button">
+               <ul class = "nav navbar-nav">
+                   <li><a id = "Team" class = "nav-link" data-toggle = "dropdown" href = "#">Team<span class = "caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a class = "sublink" href = "about_team.html">Team Members</a></li>
+                            <li><a class = "sublink" href = "sponsors.html">Sponsors</a></li>
+                       </ul>
+                   </li>
+                   <li><a id = "Project" class = "nav-link" data-toggle = "dropdown" href = "#">Projects<span class = "caret"></span></a>
+                       <ul class = "dropdown-menu">
+                           <li><a class = "sublink sublink-head" href = "#">Overview of Projects</a></li>
+                           <li><a class = "sublink" href = "../projects/project_Lyme.html">A Living Vaccine</a></li>
+                           <li><a class = "sublink" href = "../projects/phage.html">Trojan Phage</a></li>
+                       </ul>
+                   </li>
+                   <li><a id = "Outreach" class = "nav-link" data-toggle = "dropdown" href = "#">Human Practices<span class = "caret"></span></a>
+                       <ul class = "dropdown-menu">
+                           <li><a class = "sublink" href = "http://2015.igem.org/Team:Rock_Ridge_Virginia/Practices">2015 Human Practices</a></li>
+                           <li><a class = "sublink" href = "../outreach/outreach.html">2016 Human Practices</a></li>
+                       </ul>
+                   </li>
+                   <li><a id = "Calendar" class = "nav-link" href = "../calendar/calendar.html">Calendar</a></li>
+               </ul>
+           </div>
+        </div>
+    </nav>-->
+    <?php
+        include /nav.php;
+    ?>
+    
+    <section class = "jumbotron page-header">
+        <h1>Our Team</h1>
+    </section>
+    
+    <section class = "container-fluid">    
+        <section><!--col-xs-1 col-lg-1-->
+            <section id = "plasmid">
+                <img usemap = "#about_map" src = "../../images/team/plas/intro.png"/>
+            </section>
+        </section>
+        
+        <div id = "faces-bank">
+            <img src="../../images/team/bank/viggy.png" alt="Vignesh Valaboju" data-name="Vignesh" />
+            <img src="../../images/team/bank/uday.png" alt="Uday Sidhu" data-name="Uday" />
+            <img src="../../images/team/bank/shrunal.png" alt="Shrunal Pothagoni" data-name="Shrunal" />
+            <img src="../../images/team/bank/gwyn.png" alt="Gwyn Schloer" data-name="Gwyn" />
+            <img src="../../images/team/bank/varun.png" alt="Varun Kulkarni" data-name="Varun" />
+            <img src="../../images/team/bank/kat.png" alt="Katarina Acosta" data-name="Katarina" />
+        </div>
+    </section>
+</body>
+</html>
