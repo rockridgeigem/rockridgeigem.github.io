6afeca754ced1058257eb747609f9a85daebc65b
Varun Kulkarni <vskred126@gmail.com>
2017-02-08 10:08:06 -0500

    foo

--- js/custom/plasma.js
+++ js/custom/plasma.js
@@ -8,17 +8,41 @@ $(document).ready(function(){
         "Uday": "../../images/team/plas/uday.png",
         "Vignesh": "../../images/team/plas/viggy.png"
     };
-    
+    var ori = {
+        "Varun" : "Varun Kulkarni.  Born in India, Junior at RRH"
+    }
     var hobbies = {
-        "Varun"  : "coding"
-    };
-    
+        "Varun" : "Programming, Tennis, and Being Awesome"
+    }
+    var positionProj = {
+        "Varun" : "iGEM's Lead Tech Specialist and Web Designer, Contributed to the Trojan Phage"
+    }
+    var whyIgem = {
+        "Varun" : "I joined iGEM because I've been interested in applied biology integrated with technology, which sounds just like iGEM"
+    }
     $("#faces img").click(function(){
         dataName = $(this).data('name');
         $("#plasmid-container img").attr("src", images[dataName]);
+        $("#plasmid-flash").hide();
+        $("#plasmid-flash").text("");
     });
     
-    $("#area-hobby").click(function(){
-        alert(hobbies[dataName]);
+    
+    
+    $("#area-hobbies").click(function(){
+        $("#plasmid-flash").text(hobbies[dataName]);
+        $("#plasmid-flash").show();
+    });
+    $("#area-ori").click(function(){
+        $("#plasmid-flash").text(ori[dataName]);
+        $("#plasmid-flash").show();
+    });
+    $("#area-position").click(function(){
+        $("#plasmid-flash").text(positionProj[dataName]);
+        $("#plasmid-flash").show();
+    });
+    $("#area-why").click(function(){
+        $("#plasmid-flash").text(whyIgem[dataName]);
+        $("#plasmid-flash").show();
     });
 });
